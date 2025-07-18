da5fecea38b4bc7d44feb51acf6b245ac5a88745
AnalystYuchels <uche4xploit@gmail.com>
2025-07-18 15:36:49 +0100

    Updated code for Card.tsx and Pills.tsx

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,7 +1,17 @@
+import React from "react";
+import Card from "@/components/Card";
+
 const Landing: React.FC = () => {
   return (
-    <div>
-      <h1 className="text-xl font-extralight">Landing Page</h1>
+    <div className="flex flex-col items-center p-5">
+      <h1 className=" text-xl font-extralight">Landing Page</h1>
+      
+      {/* Single Card */}
+      <Card />
+
+      {/* Multiple Cards */}
+      <Card />
+      <Card />
     </div>
   );
 };
